0ad9b0cc28d0089529a7745fbf2ec554a7e0cb2a
KwonEunchan <klg3377@naver.com>
2022-09-22 09:48:05 +0900

    initial set

---
+++ project/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <form action="/hi" method="get">
+        <input type="submit"/>
+      </form>
+    </div>
+  );
+}
+
+export default App;
